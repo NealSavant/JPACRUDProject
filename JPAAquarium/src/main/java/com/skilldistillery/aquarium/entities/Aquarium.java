1a14d87b7f6857691bdfc34d6c78878745b7b868
Neal Savant <nealrsavant@gmail.com>
2020-01-17 14:56:13 -0700

    gradle setup

---
+++ JPAAquarium/src/main/java/com/skilldistillery/aquarium/entities/Aquarium.java
@@ -0,0 +1,54 @@
+package com.skilldistillery.aquarium.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//class name matches table name in db
+
+@Entity
+public class Aquarium {
+
+	// F I E L D S
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int id;
+	private String name;
+
+	// C O N S T R U C T O R
+	public Aquarium(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public Aquarium() {
+
+	}
+
+	// G E T T E R S / S E T T E R S
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Aquarium [id=").append(id).append(", name=").append(name).append("]");
+		return builder.toString();
+	}
+
+}
